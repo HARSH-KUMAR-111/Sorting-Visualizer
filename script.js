dc1d32446afd2656a70e545e0ce2cbfa37a126a5
Harsh kumar <hk342064@gmail.com>
2024-02-04 23:59:30 +0530

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const n=10;
+const n=50;
 const array=[];
 
 init();
@@ -9,29 +9,50 @@ function init(){
   showBars();
 }
 function play(){
-  bubbleSort(array);
-  showBars();
+  const copy=[...array];
+  const swaps=bubbleSort(copy);
+  animate(swaps);
+  //showBars();
+}
+function animate(swaps){
+  if(swaps.length==0){
+    showBars();
+    return;
+  }
+  const [i,j]=swaps.shift();
+  [array[i],array[j]]=[array[j],array[i]];
+  showBars([i,j]);
+  setTimeout(function(){
+    animate(swaps);
+  },50);
 }
 // console.log(arr);
 function bubbleSort(array){
+  const swaps=[];
   do{
     var swapped=false;
     for(let i=1;i<array.length;i++){
       if(array[i-1]>array[i]){
         swapped=true;
+        swaps.push([i-1,i]);
         [array[i-1],array[i]]=[array[i],array[i-1]];
       }
     }
   }while(swapped);
+  return swaps;
 }
 
 
-function showBars(){
+function showBars(indices){
   container.innerHTML="";
   for(let i=0;i<array.length;i++){
     const bar=document.createElement("div");
     bar.style.height=array[i]*100+"%";
     bar.classList.add("bar");
+
+    if(indices && indices.includes(i)){
+      bar.style.backgroundColor="red";
+    }
     container.appendChild(bar);
   }
 }
